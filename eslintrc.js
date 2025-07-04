b3e9aff6928e242d44a2683ce552c7b413a45e8b
cho-media <nicholas@chomedia.au>
2025-07-05 01:51:11 +1000

    Update eslintrc.js

--- eslintrc.js
+++ eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
 	parserOptions: {
 		project: 'tsconfig.json',
 		sourceType: 'module',
-		extraFileExtensions: ['.json'],
 	},
 	plugins: ['@typescript-eslint'],
 	extends: [
@@ -18,14 +17,15 @@ module.exports = {
 		'plugin:n8n-nodes-base/community',
 	],
 	rules: {
-		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-		'@typescript-eslint/no-explicit-any': 'warn',
+		'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-non-null-assertion': 'off',
 		'@typescript-eslint/ban-ts-comment': 'off',
-		'prefer-const': 'error',
-		'no-var': 'error',
+		'prefer-const': 'warn',
+		'no-var': 'warn',
+		'no-console': 'off',
 	},
 	ignorePatterns: ['dist/**', 'node_modules/**'],
 };
